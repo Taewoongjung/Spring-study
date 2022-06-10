6ce21f2005e50af3e61596775c8a1c0f18d27ccc
Taewoongjung <aipooh8882@naver.com>
2022-06-11 02:36:37 +0900

    test code of MemberServiceImpl. However need to think about violation of DIP

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,8 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository(); // 이렇게 하면 추상화에도 의존하고 구현체에도 의존한다. 고로 DIP 를 위반하고 있다.
+                                                                                    // 이것을 DIP 를 위반 안하게 하려면..? 고민해보자.
 
     @Override
     public void join(Member member) {
