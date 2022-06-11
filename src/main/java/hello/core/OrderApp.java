916c9cbae097651452a5ba2d20a13487638a8ee4
Taewoongjung <aipooh8882@naver.com>
2022-06-11 15:05:34 +0900

    order unit test

---
+++ src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,16 @@
+package hello.core;
+
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1L;
+    }
+}
